72bc1be832bce0c87e3c652253501509dd667df6
daveyrpt <89343504+daveyrpt@users.noreply.github.com>
2022-12-23 01:40:56 +0800

    update delete modal with bootstrap

--- resources/views/newsletters/admin/index.blade.php
+++ resources/views/newsletters/admin/index.blade.php
@@ -66,23 +66,36 @@
             </div>
               <p>Publish status : {{ $newsletter->published ? 'Yes' : 'No' }}</p>
               
-              <div class="btn-group" >     
-                  <a href="{{ route('newsletters.edit', $newsletter) }}" class=""><button class="btn btn-secondary" type="submit">Edit</button></a>
-                  <a href="#" onclick="return confirmation()">
-                  <form action="{{ route('newsletters.destroy', $newsletter) }}" class="mx-2" method="post">
-                      @csrf
-                      @method('delete')
-                      <button class="btn btn-danger" type="submit">Delete</button>
+              
+              <div class="btn-group" >    
+                    
+                    <!-- Edit Button  -->
+                    <a href="{{ route('newsletters.edit', $newsletter) }}" class=""><button class="btn btn-secondary" type="submit">Edit</button></a>
 
-                  </form>   
-                  </a>
-                  <!-- Model Delete Confirmation -->
-                  <script type="text/javascript">
-                        function confirmation() {
-                        return confirm('Are you sure you want to remove this newsletter?');
-                        }
-                   </script>  
-
+                    <!-- Delete Button  -->
+                    <form action="{{ route('newsletters.destroy', $newsletter) }}" class="mx-2" method="post">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button">Delete</button>
+                        <!-- Delete Button Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    Are you sure want to delete this newsletter? You can restore it later on.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                        <button type="submit" class="btn btn-primary">Proceed</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>  
                </div>
 
         @endforeach
